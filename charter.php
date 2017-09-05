76da5f83e6d0d5342ef254fe4ebbce7de7f99e7e
amandadyrvig <amandadyrvig@hotmail.com>
2017-09-05 12:58:42 +0200

    footer include

--- charter.php
+++ charter.php
@@ -104,7 +104,7 @@
       </section>
   </article>
 </main>
-    <script src="https://unpkg.com/vue/dist/vue.js"></script>
+    <?php include 'inc/footer.inc.php'; ?>
     <script src="js/charter-checklist.js"></script>
     <script src="js/charter-newlist.js"></script>
   </body>

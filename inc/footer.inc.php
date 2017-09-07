dc922a9dd948f2c66a3878a27c5f5a36d04c4028
Carsten <carstena1994@gmail.com>
2017-09-07 10:06:42 +0200

    bugs

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -1,5 +1,5 @@
 <script src="https://unpkg.com/vue/dist/vue.js"></script>
 <script src="https://use.fontawesome.com/e367f3d54f.js"></script>
-<script src="js/ski-script.js">
+<script src="js/ski-script.js"></script>
 </body>
 </html>

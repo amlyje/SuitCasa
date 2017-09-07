b4553f6318fd5399761d395ca60c318d0ef9abf8
Carsten <carstena1994@gmail.com>
2017-09-07 15:22:43 +0200

    Rettet alt styling
    Gjort klar til at tilføje onclick funktion til at skifte screen

--- inc/head.inc.php
+++ inc/head.inc.php
@@ -7,13 +7,12 @@
   <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Lato:300" rel="stylesheet">
   <link rel="stylesheet" href="css/reset.css">
-  <link rel="stylesheet" href="css/style.css"> <!--forside, login og signup -->
   <link rel="stylesheet" href="css/createnew.min.css">
   <link rel="stylesheet" href="css/charter.css">
   <link rel="stylesheet" href="css/cityStyle.css">
   <link rel="stylesheet" href="css/ski-style.css">
   <link rel="stylesheet" href="css/ski-style-mq.css">
-
+  <link rel="stylesheet" href="css/style.css"> <!--forside, login og signup -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 </body>
--- index.php
+++ index.php
@@ -4,30 +4,31 @@
 <!-- 1. Autocomplete -->
 <!-- 2. Add new list -->
 
-<header>
-    <a href="index.php" class="back"></a>
-    <div class="login_buttons">
-        <a href="login.php"><button type="button" name="Login">Login</button></a>
-        <a href="sign-up.php"><button type="button" name="Sign-up">Sign up</button></a>
-    </div>
-</header>
-<a href="index.php"><h1 class="fp">SuitCasa</h1></a>
-<section class="intro">
-    <p>Get inspiration for your next travel packing list and create, save and share your own.</p>
+<section id="landing-screen">
+  <header>
+      <a href="index.php" class="back"></a>
+      <div class="login_buttons">
+          <a href="login.php"><button type="button" name="Login">Login</button></a>
+          <a href="sign-up.php"><button type="button" name="Sign-up">Sign up</button></a>
+      </div>
+  </header>
+  <a href="index.php"><h1 class="fp">SuitCasa</h1></a>
+  <section class="intro">
+      <p>Get inspiration for your next travel packing list and create, save and share your own.</p>
+  </section>
+  <section class="frontpage-buttons">
+      <a href="createnew.php"><button class="dropbtn" type="button" name="Opret pakkeliste">Create your own list</button></a>
+      <div class="dropdown">
+          <button onclick="myFunction()" class="dropbtn">Inspire me!</button>
+          <div id="myDropdown" class="dropdown-content">
+              <a href="city.php">City Holiday</a>
+              <a href="charter.php">Charter</a>
+              <a href="skiing.php">Skiing</a>
+          </div>
+      </div>
+  </section>
+  <section class="background"></section>
 </section>
-<section class="frontpage-buttons">
-    <a href="createnew.php"><button class="dropbtn" type="button" name="Opret pakkeliste">Create your own list</button></a>
-    <div class="dropdown">
-        <button onclick="myFunction()" class="dropbtn">Inspire me!</button>
-        <div id="myDropdown" class="dropdown-content">
-            <a href="city.php">City Holiday</a>
-            <a href="charter.php">Charter</a>
-            <a href="skiing.php">Skiing</a>
-        </div>
-    </div>
-</section>
-<section class="background"></section>
-
 
 <?php  include 'createnew.php' ?>
 <?php  include 'skiing.php' ?>

c334ffd0555de0dedd716078b94121eaf92a0f8d
Carsten <carstena1994@gmail.com>
2017-09-08 13:53:31 +0200

    style changes

--- inc/head.inc.php
+++ inc/head.inc.php
@@ -6,6 +6,7 @@
   <link href="https://fonts.googleapis.com/css?family=Raleway:200;300;400" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Lato:300" rel="stylesheet">
+  <link rel="stylesheet" href="css/animate.css">
   <link rel="stylesheet" href="css/reset.css">
   <link rel="stylesheet" href="css/createnew.min.css">
   <link rel="stylesheet" href="css/charter.css">
@@ -13,7 +14,6 @@
   <link rel="stylesheet" href="css/ski-style.css">
   <link rel="stylesheet" href="css/ski-style-mq.css">
   <link rel="stylesheet" href="css/style.css"> <!--forside, login og signup -->
-  <link rel="stylesheet" href="css/animate.css">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
